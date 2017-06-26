1227f435b0678e92329415b5b2c1f01989b77be8
David Andersen <git@davidandersen.us>
2017-06-25 22:59:15 -0700

    initial

---
+++ Assets/Cube.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Savoa;
+
+public class Cube : MonoBehaviour
+{
+    private PositionComponent positionComponent;
+    private Vector3 vector3;
+
+    void Start()
+    {
+        EngineRunner engineRunner = GameObject.Find("EngineRunner").GetComponent<EngineRunner>();
+        Entity entity = new Entity();
+        positionComponent = new PositionComponent();
+        entity.AddComponent(positionComponent);
+        engineRunner.engine.AddEntity(entity);
+    }
+
+    void Update()
+    {
+        vector3.Set(positionComponent.X, positionComponent.Y, 0);
+        transform.position = vector3;
+    }
+}
+
+class CubeSystem : IteratingEntitySystem
+{
+    public override void AddedToEngine(Engine engine)
+    {
+        entities = engine.EntitiesFor(new Family(typeof(PositionComponent)));
+    }
+
+    public override void processEntity(Entity entity)
+    {
+        PositionComponent position = entity.GetComponent<PositionComponent>();
+        position.X++;
+        position.Y++;
+    }
+}
+
+class PositionComponent
+{
+    public float X;
+    public float Y;
+}
---
+++ Assets/EngineRunner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Savoa;
+
+public class EngineRunner : MonoBehaviour
+{
+    public Engine engine = new Engine();
+
+    void Start()
+    {
+        engine.AddSystem(new CubeSystem());
+    }
+
+    void Update()
+    {
+        engine.Process();
+        //Debug.Log("update");
+    }
+}
